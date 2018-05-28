3a332cda06dc356bdb4f0ca015e724d7216978f8
Carl Naso <carlnaso@nasomedia.com>
2018-05-27 20:41:32 -0700

    Remove background from LoginPage.

--- Carmelo.Word/Pages/LoginPage.xaml.cs
+++ Carmelo.Word/Pages/LoginPage.xaml.cs
@@ -5,7 +5,7 @@ namespace Carmelo.Word.Pages
     /// <summary>
     /// Interaction logic for LoginPage.xaml
     /// </summary>
-    public partial class LoginPage : Page
+    public partial class LoginPage : BasePage
     {
         public LoginPage()
         {
