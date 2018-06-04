32c7325b92d48ce11b7672d3db04330298a5f5e0
Carl Naso <carlnaso@nasomedia.com>
2018-06-03 21:06:56 -0700

    Exposes a SecureString for the inheriting class, LoginPage, to be used for binding.

---
+++ Carmelo.Word/ViewModels/Base/ISecurePassword.cs
@@ -0,0 +1,12 @@
+﻿using System.Security;
+
+namespace Carmelo.Word.ViewModels
+{
+    /// <summary>
+    /// Interface provides a <see cref="SecureString"/> to the inheriting class.
+    /// </summary>
+    public interface ISecurePassword
+    {
+        SecureString Password { get; }
+    }
+}
