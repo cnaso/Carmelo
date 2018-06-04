c8d5f1cbc7ee824fd07fe9cc2ed3c234abeb2a06
Carl Naso <carlnaso@nasomedia.com>
2018-06-03 21:10:53 -0700

    Hash extension for SecureString. Uses BCrypt to hash the password.

---
+++ Carmelo.Word/Extensions/SecureStringExtension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace Carmelo.Word.Extensions
+{
+    /// <summary>
+    /// Convenience extensions for <see cref="SecureString"/>.
+    /// </summary>
+    public static class SecureStringExtension
+    {
+        /// <summary>
+        /// Hashes a <see cref="SecureString"/>.
+        /// </summary>
+        /// <param name="secureString"></param>
+        /// <returns></returns>
+        public static string Hash(this SecureString secureString)
+        {
+            if (secureString == null)
+            {
+                return string.Empty;
+            }
+
+            var unmanagedString = IntPtr.Zero;
+
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(secureString);
+                string hash = BCrypt.Net.BCrypt.HashPassword(Marshal.PtrToStringUni(unmanagedString), 14);
+
+                return hash;
+            }
+            catch (Exception e)
+            {
+                
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+
+            return string.Empty;
+        }
+    }
+}
