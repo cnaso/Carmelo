d7508e5bd04fdc75423634f10eb60ebf8a903c95
Carl Naso <carlnaso@nasomedia.com>
2018-06-03 21:11:36 -0700

    Added to handle parameterized commands.

---
+++ Carmelo.Word/ViewModels/Base/RelayParameterizedCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Carmelo.Base.ViewModels
+{
+    /// <summary>
+    /// Class handles firing parameterized commands implemented from the <see cref="ICommand"/> interface.
+    /// </summary>
+    public class RelayParameterizedCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action<object> action;
+
+        public RelayParameterizedCommand(Action<object> action)
+        {
+            this.action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+    }
+}
