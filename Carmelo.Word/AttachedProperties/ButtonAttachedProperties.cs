2640d54e21df9e5474ccbe85551ba643a54d6611
Carl Naso <carlnaso@nasomedia.com>
2018-06-04 21:27:51 -0700

    Added to handle button state.

---
+++ Carmelo.Word/AttachedProperties/ButtonAttachedProperties.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Carmelo.Word.AttachedProperties
+{
+    /// <summary>
+    /// IsBusy attached property for the <see cref="Button"/> control.
+    /// Used to remove placeholder text when the password length is greater than zero.
+    /// </summary>
+    public class IsBusyProperty : BaseAttachedProperty<IsBusyProperty, bool>
+    {
+        
+    }
+}
