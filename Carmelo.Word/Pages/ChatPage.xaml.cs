8cf4a0ba458e2776e21dd2bb04ea1fa1b830edfb
Carl Naso <carlnaso@nasomedia.com>
2018-06-08 22:40:59 -0700

    Added page for the Chat window.

---
+++ Carmelo.Word/Pages/ChatPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Carmelo.Word.ViewModels;
+
+namespace Carmelo.Word.Pages
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class ChatPage : BasePage<LoginViewModel>
+    {
+        public ChatPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
