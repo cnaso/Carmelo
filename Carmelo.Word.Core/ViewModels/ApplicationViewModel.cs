86ef38ee8a9e1958362ea942f539a4f5898f055e
Carl Naso <carlnaso@nasomedia.com>
2018-06-19 11:48:53 -0700

    Added view model for App.

---
+++ Carmelo.Word.Core/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Carmelo.Word.Core.DataModels;
+using Carmelo.Word.Core.ViewModels.Base;
+
+namespace Carmelo.Word.Core.ViewModels
+{
+    /// <summary>
+    /// Application view state.
+    /// </summary>
+    public class ApplicationViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// Current page to be displayed in the application.
+        /// </summary>
+        public ApplicationPage CurrentPage { get; set; } = ApplicationPage.Login;
+    }
+}
