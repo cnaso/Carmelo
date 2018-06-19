adaeacd6549e4ba404efb5808a070b0477c402ef
Carl Naso <carlnaso@nasomedia.com>
2018-06-19 11:39:41 -0700

    Added to handle IoC view model retrieval.

---
+++ Carmelo.Word/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,18 @@
+﻿using Carmelo.Word.Core.IoC;
+using Carmelo.Word.Core.ViewModels;
+
+namespace Carmelo.Word.ViewModels
+{
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Singleton instance of the locator.
+        /// </summary>
+        public static ViewModelLocator Instance { get; private set; } = new ViewModelLocator();
+
+        /// <summary>
+        /// Application view model.
+        /// </summary>
+        public static ApplicationViewModel ApplicationViewModel => IocContainer.Get<ApplicationViewModel>();
+    }
+}
