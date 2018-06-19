e38dc12bcd37b4ea29b3bddc217634411a49a869
Carl Naso <carlnaso@nasomedia.com>
2018-06-19 11:43:24 -0700

    Added OnStartup override for IoC container configuration.

--- Carmelo.Word/App.xaml.cs
+++ Carmelo.Word/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Carmelo.Word.Core.IoC;
 using System.Windows;
 
 namespace Carmelo.Word
@@ -13,5 +8,18 @@ namespace Carmelo.Word
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Startup to load IoC container for the application.
+        /// </summary>
+        /// <param name="e">Event argument object</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            IocContainer.Configure();
+
+            Current.MainWindow = new MainWindow();
+            Current.MainWindow.Show();
+        }
     }
 }
