1fe51c2f76c467e0afee88ea3d77df1320d33860
Carl Naso <carlnaso@nasomedia.com>
2018-06-19 11:44:51 -0700

    Added for dependency injection with NInject.

---
+++ Carmelo.Word.Core/IoC/IocContainer.cs
@@ -0,0 +1,43 @@
+﻿using Ninject;
+using Carmelo.Word.Core.ViewModels;
+
+namespace Carmelo.Word.Core.IoC
+{
+    /// <summary>
+    /// Inversion of Control container for the application.
+    /// </summary>
+    public static class IocContainer
+    {
+        /// <summary>
+        /// IoC <see cref="StandardKernel"/> for the application.
+        /// </summary>
+        public static IKernel Kernel { get; private set; } = new StandardKernel();
+
+        /// <summary>
+        /// Configures the IoC container by binding the required classes for the application.
+        /// <para>Note: Must be called on application startup.</para>
+        /// </summary>
+        public static void Configure()
+        {
+            BindViewModels();
+        }
+
+        /// <summary>
+        /// Get's a service from the IoC, of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to get.</typeparam>
+        /// <returns></returns>
+        public static T Get<T>()
+        {
+            return Kernel.Get<T>();
+        }
+
+        /// <summary>
+        /// Binds the view models to the IoC container kernal.
+        /// </summary>
+        private static void BindViewModels()
+        {
+            Kernel.Bind<ApplicationViewModel>().ToConstant(new ApplicationViewModel());
+        }
+    }
+}
