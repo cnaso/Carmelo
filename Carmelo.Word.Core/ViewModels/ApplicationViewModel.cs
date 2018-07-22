c321801b75eb4cd2a720dd9ff95a04c7a045f9a1
Carl Naso <carlnaso@nasomedia.com>
2018-07-22 13:41:18 -0700

    Added GoToPage method which shows the SideList if the page is the Chat page.

--- Carmelo.Word.Core/ViewModels/ApplicationViewModel.cs
+++ Carmelo.Word.Core/ViewModels/ApplicationViewModel.cs
@@ -11,11 +11,22 @@ namespace Carmelo.Word.Core.ViewModels
         /// <summary>
         /// Current page to be displayed in the application.
         /// </summary>
-        public ApplicationPage CurrentPage { get; set; } = ApplicationPage.Login;
+        public ApplicationPage CurrentPage { get; private set; } = ApplicationPage.Login;
 
         /// <summary>
         /// Side List to be visible or not.
         /// </summary>
         public bool SideListVisible { get; set; } = false;
+
+        /// <summary>
+        /// Navigate to the specified page.
+        /// </summary>
+        /// <param name="page"></param>
+        public void GoToPage(ApplicationPage page)
+        {
+            CurrentPage = page;
+
+            SideListVisible = page == ApplicationPage.Chat;
+        }
     }
 }
