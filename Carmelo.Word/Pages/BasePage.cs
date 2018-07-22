ab0fb49de9a6592df9c0fd5f93628b0b506369d5
Carl Naso <carlnaso@nasomedia.com>
2018-07-22 13:45:14 -0700

    Added BasePage without VM, BasePage<VM> inherits from BasePage now.

--- Carmelo.Word/Pages/BasePage.cs
+++ Carmelo.Word/Pages/BasePage.cs
@@ -9,8 +9,9 @@ namespace Carmelo.Word.Pages
     /// <summary>
     /// Base page for all pages to inherit for animation functionality.
     /// </summary>
-    public class BasePage<VM> : Page where VM : BaseViewModel, new()
+    public class BasePage : Page
     {
+        #region Public Properties
         /// <summary>
         /// Animation to play when page is loaded.
         /// </summary>
@@ -24,8 +25,94 @@ namespace Carmelo.Word.Pages
         /// <summary>
         /// Seconds the animation takes to complete.
         /// </summary>
-        public float SlideSeconds { get; set; } = 1.0f;
+        public float SlideSeconds { get; set; } = 0.5f;
 
+        /// <summary>
+        /// Flag to indicate if page should animate out on load. Used when moving page to another frame.
+        /// </summary>
+        public bool ShouldAnimateOut { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Default constructor sets the inheriting pages <see cref="Visibility"/> to Collapsed initally if the <see cref="PageAnimation"/> is None.
+        /// </summary>
+        public BasePage()
+        {
+            if (PageLoadAnimation != PageAnimation.None)
+            {
+                Visibility = Visibility.Collapsed;
+            }
+
+            Loaded += BasePage_Loaded;
+        }
+
+        #region Public Methods
+        /// <summary>
+        /// Task loads the animation to display the page depending on the <see cref="PageAnimation"/> type.
+        /// </summary>
+        /// <returns></returns>
+        public async Task AnimateIn()
+        {
+            if (PageLoadAnimation == PageAnimation.None)
+            {
+                return;
+            }
+
+            switch (PageLoadAnimation)
+            {
+                case PageAnimation.SlideAndFadeInFromRight:
+                    await this.SlideAndFadeInFromRight(SlideSeconds);
+
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Task loads the animation to remove the page depending on the <see cref="PageAnimation"/> type.
+        /// </summary>
+        /// <returns></returns>
+        public async Task AnimateOut()
+        {
+            if (PageUnLoadAnimation == PageAnimation.None)
+            {
+                return;
+            }
+
+            switch (PageUnLoadAnimation)
+            {
+                case PageAnimation.SlideAndFadeOutToLeft:
+                    await this.SlideAndFadeOutToLeft(SlideSeconds);
+
+                    break;
+            }
+        } 
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Event that calls the <see cref="AnimateIn"/> task.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void BasePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ShouldAnimateOut)
+            {
+                await AnimateOut();
+            }
+            else
+            {
+                await AnimateIn();
+            }
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Base page with added ViewModel support.
+    /// </summary>
+    public class BasePage<VM> : BasePage where VM : BaseViewModel, new()
+    {
         /// <summary>
         /// View Model for the page. Updates the DataContext with the VM object.
         /// </summary>
@@ -50,68 +137,11 @@ namespace Carmelo.Word.Pages
         private VM viewModel;
 
         /// <summary>
-        /// Default constructor sets the inheriting pages <see cref="Visibility"/> to Collapsed initally if the <see cref="PageAnimation"/> is None.
-        /// Also sets the inheriting pages Loaded event and creates a new <see cref="VM"/>.
+        /// Default constructor calls the inherited <see cref="BasePage"/> constructor then sets the <see cref="VM"/>.
         /// </summary>
-        public BasePage()
+        public BasePage() : base()
         {
-            if (PageLoadAnimation != PageAnimation.None)
-            {
-                Visibility = Visibility.Collapsed;
-            }
-
-            Loaded += BasePage_Loaded;
             ViewModel = new VM();
-        }
-
-        /// <summary>
-        /// Event that calls the <see cref="AnimateIn"/> task.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void BasePage_Loaded(object sender, RoutedEventArgs e)
-        {
-            await AnimateIn();
-        }
-
-        /// <summary>
-        /// Task loads the animation to display the page depending on the <see cref="PageAnimation"/> type.
-        /// </summary>
-        /// <returns></returns>
-        private async Task AnimateIn()
-        {
-            if (PageLoadAnimation == PageAnimation.None)
-            {
-                return;
-            }
-
-            switch (PageLoadAnimation)
-            {
-                case PageAnimation.SlideAndFadeInFromRight:
-                    await PageAnimationHelpers.SlideAndFadeInFromRight(this, SlideSeconds);
-
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// Task loads the animation to remove the page depending on the <see cref="PageAnimation"/> type.
-        /// </summary>
-        /// <returns></returns>
-        private async Task AnimateOut()
-        {
-            if (PageUnLoadAnimation == PageAnimation.None)
-            {
-                return;
-            }
-
-            switch (PageUnLoadAnimation)
-            {
-                case PageAnimation.SlideAndFadeOutToLeft:
-                    await PageAnimationHelpers.SlideAndFadeOutToLeft(this, SlideSeconds);
-
-                    break;
-            }
         }
     }
 }
