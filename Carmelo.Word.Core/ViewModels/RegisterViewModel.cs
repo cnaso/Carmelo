a1420af5597998d0cbeee28dc2451529d6bef507
Carl Naso <carlnaso@nasomedia.com>
2018-07-22 19:31:17 -0700

    Add RegisterPage.

---
+++ Carmelo.Word.Core/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows.Input;
+using System.Threading.Tasks;
+using Carmelo.Word.Core.ViewModels.Base;
+using Carmelo.Word.Core.Commands;
+using Carmelo.Word.Core.Extensions;
+using Carmelo.Word.Core.IoC;
+using Carmelo.Word.Core.DataModels;
+
+namespace Carmelo.Word.Core.ViewModels
+{
+    /// <summary>
+    /// View Model to handle the <see cref="RegisterPage"/>.
+    /// </summary>
+    public class RegisterViewModel : BaseViewModel
+    {
+        public string Email { get; set; }
+
+        public bool RegisterInProgress { get; set; }
+
+        public ICommand LoginCommand { get; set; }
+
+        public ICommand RegisterCommand { get; set; }
+
+        public RegisterViewModel()
+        {
+            LoginCommand = new RelayCommand(async () => await Login());
+            RegisterCommand = new RelayParameterizedCommand(async (parameter) => await Register(parameter));
+        }
+
+        /// <summary>
+        /// Registers the user account.
+        /// </summary>
+        /// <param name="parameter"><see cref="SecureString"/> passed in from the view for the users password.</param>
+        /// <returns></returns>
+        public async Task Register(object parameter)
+        {
+            await RunCommand(() => RegisterInProgress, async () =>
+            {
+                await Task.Delay(5000);
+                var hash = (parameter as ISecurePassword).Password.Hash();
+            });
+        }
+
+        /// <summary>
+        /// Takes user to the <see cref="LoginPage"/>.
+        /// </summary>
+        /// <param name="parameter">Object containing register information.</param>
+        /// <returns></returns>
+        public async Task Login()
+        {
+            IocContainer.Get<ApplicationViewModel>().GoToPage(ApplicationPage.Login);
+        }
+    }
+}
---
+++ Carmelo.Word/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Carmelo.Word.Core.ViewModels;
+using Carmelo.Word.Core.ViewModels.Base;
+using System.Security;
+
+namespace Carmelo.Word.Pages
+{
+    /// <summary>
+    /// Interaction logic for RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterPage : BasePage<RegisterViewModel>, ISecurePassword
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The <see cref="SecureString"/> password inherited from the <see cref="ISecurePassword"/> interface.
+        /// </summary>
+        public SecureString Password => PasswordText.SecurePassword;
+    }
+}
