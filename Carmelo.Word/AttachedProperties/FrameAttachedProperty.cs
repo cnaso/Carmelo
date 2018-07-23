04491407a0e27a1c69c991faf35f48e2e2302ead
Carl Naso <carlnaso@nasomedia.com>
2018-07-22 19:31:59 -0700

    Add attached property from previous commit. Forgot to add then.

---
+++ Carmelo.Word/AttachedProperties/FrameAttachedProperty.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Carmelo.Word.AttachedProperties
+{
+    /// <summary>
+    /// NoFrameHistory attached property for the <see cref="Frame"/> control.
+    /// This is done to remove the navigation UI from coming up on the frame and keeps
+    /// the pages navigated to history cleared so the keyboard shortcuts for the navigation
+    /// UI are of no use.
+    /// </summary>
+    public class NoFrameHistory : BaseAttachedProperty<NoFrameHistory, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs eventArg)
+        {
+            var frame = (sender as Frame);
+
+            frame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+            frame.Navigated += (ss, ee) => ((Frame)ss).NavigationService.RemoveBackEntry();
+        }
+    }
+}
