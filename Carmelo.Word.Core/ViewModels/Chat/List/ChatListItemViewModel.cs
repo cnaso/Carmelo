1130b77faea75aeebfce6c2d0a2911694034b598
Carl Naso <carlnaso@nasomedia.com>
2018-07-28 21:45:20 -0700

    Added comments for view models.

--- Carmelo.Word.Core/ViewModels/Chat/List/ChatListItemViewModel.cs
+++ Carmelo.Word.Core/ViewModels/Chat/List/ChatListItemViewModel.cs
@@ -1,20 +1,38 @@
 ﻿namespace Carmelo.Word.Core
 {
     /// <summary>
-    /// View Model for the <see cref="ChatListItemControl"/> user control.
+    /// View Model for the chat list items user control.
     /// </summary>
     public class ChatListItemViewModel : BaseViewModel
     {
+        /// <summary>
+        /// Users displayed name from the chat.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Users last message from the chat.
+        /// </summary>
         public string Message { get; set; }
 
+        /// <summary>
+        /// Users displayed name initials.
+        /// </summary>
         public string Initials { get; set; }
 
+        /// <summary>
+        /// Users displayed initial background color.
+        /// </summary>
         public string ProfilePictureRGB { get; set; }
 
+        /// <summary>
+        /// User has new message pending.
+        /// </summary>
         public bool NewContentAvailable { get; set; }
 
+        /// <summary>
+        /// User is selected or not in the list.
+        /// </summary>
         public bool IsSelected { get; set; }
     }
 }
--- Carmelo.Word.Core/ViewModels/Chat/List/ChatListViewModel.cs
+++ Carmelo.Word.Core/ViewModels/Chat/List/ChatListViewModel.cs
@@ -3,10 +3,13 @@
 namespace Carmelo.Word.Core
 {
     /// <summary>
-    /// View Model for the <see cref="ChatListControl"/> user control.
+    /// View Model for the chat message list user control.
     /// </summary>
     public class ChatListViewModel : BaseViewModel
     {
+        /// <summary>
+        /// List of user chat itmes.
+        /// </summary>
         public List<ChatListItemViewModel> Items { get; set; }
     }
 }
